4d4f4d225d5907dc3ef87b21aeb215e6ff438646
m-glock <mareike.glock@t-online.de>
2020-11-22 20:31:00 +0100

    added test file for FoodApiAccess and defined some tests without implementing them

---
+++ test/FoodApiAccess_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Scan a barcode and return a product object', () {
+    throw UnimplementedError('test not implemented yet');
+  });
+
+  test('Scan a barcode for a product that does not exist', () {
+    throw UnimplementedError('test not implemented yet');
+  });
+
+  test('Get all values for the tag allergens', () {
+    throw UnimplementedError('test not implemented yet');
+  });
+
+  test('Get all values for the tag allergies which does not exist in the database', () {
+    throw UnimplementedError('test not implemented yet');
+  });
+
+  test('translate a list of ingredients to german', () {
+    throw UnimplementedError('test not implemented yet');
+  });
+
+  test('translate a list of ingredients to a lnaguag that is not included in the api', () {
+    throw UnimplementedError('test not implemented yet');
+  });
+}
