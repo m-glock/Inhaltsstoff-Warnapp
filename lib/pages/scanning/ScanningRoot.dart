82ff1d5aadaa3290f6fc6f40d0f28b42e54f53aa
Kevin Wrede <kevinwrede@gmx.net>
2020-11-23 16:27:41 +0100

    #17 Barcade package added, Barcode scan example for ScanningRoot View

--- lib/pages/scanning/ScanningRoot.dart
+++ lib/pages/scanning/ScanningRoot.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanningRoot extends StatefulWidget {
   const ScanningRoot({ Key key }) : super(key: key);
@@ -8,28 +11,70 @@ class ScanningRoot extends StatefulWidget {
 }
 
 class _ScanningRootState extends State<ScanningRoot> {
+  String _scanBarcode = 'Unbekannt';
   TextEditingController _textController;
 
   @override
   void initState() {
     super.initState();
     _textController = TextEditingController(
-      text: "ScanningRoot",
+      text: "Scanne den gewünschten Artikel",
     );
+  }
+
+  startBarcodeScanStream() async {
+    FlutterBarcodeScanner.getBarcodeStreamReceiver(
+            "#ff6666", "Abbrechen", true, ScanMode.BARCODE)
+        .listen((barcode) => print(barcode));
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          "#ff6666", "Abbrechen", true, ScanMode.BARCODE);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ScanningRoot'),
+        title: Text('Barcode Scanner'),
         backgroundColor: Colors.blue,
       ),
       backgroundColor: Colors.white,
       body: Container(
         padding: const EdgeInsets.all(32.0),
         alignment: Alignment.center,
-        child: TextField(controller: _textController),
+        child: Flex(
+                      direction: Axis.vertical,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        TextField(controller: _textController),
+                        RaisedButton(
+                            onPressed: () => scanBarcodeNormal(),
+                            child: Text("Barcode scan")),
+                        RaisedButton(
+                            onPressed: () => startBarcodeScanStream(),
+                            child: Text("Barcode scan stream")),
+                        Text('Scan Ergebnis : $_scanBarcode\n',
+                            style: TextStyle(fontSize: 20))
+                      ])
       ),
     );
   }
