fc4c6ca896676287306bd0be7ba5f9f81438c493
m-glock <mareike.glock@t-online.de>
2020-11-29 10:33:22 +0100

    added all table names to DbTables enum

--- lib/database/tables/DbTables.dart
+++ lib/database/tables/DbTables.dart
@@ -3,28 +3,50 @@ import 'package:Inhaltsstoff_Warnapp/database/tables/IngredientGroup.dart';
 import 'package:Inhaltsstoff_Warnapp/database/tables/Ingredients.dart';
 
 enum DbTables{
-  ingredients,
-  ingredientGroup,
+  ingredient,
+  ingredientType,
+  type,
+  preferenceType,
+  productIngredient,
+  scanResult,
+  product,
+  list,
+  productList
 }
 
 extension DbTablesExtension on DbTables {
 
   String get name {
     switch (this) {
-      case DbTables.ingredientGroup:
-        return 'Ingredient_group';
-      case DbTables.ingredients:
-        return 'Ingredients';
+      case DbTables.ingredient:
+        return 'ingredient';
+      case DbTables.ingredientType:
+        return 'ingredienttype';
+      case DbTables.type:
+        return 'type';
+      case DbTables.preferenceType:
+        return 'preferencetype';
+      case DbTables.productIngredient:
+        return 'productingredient';
+      case DbTables.scanResult:
+        return 'scanresult';
+      case DbTables.product:
+        return 'product';
+      case DbTables.list:
+        return 'list';
+      case DbTables.productList:
+        return 'productlist';
       default:
         return null;
     }
   }
 
+  //TODO: add mapping for all classes
   DbObject fromMap(Map<String, dynamic> data){
     switch(this){
-      case DbTables.ingredientGroup:
+      case DbTables.ingredientType:
         return IngredientGroup.fromMap(data);
-      case DbTables.ingredients:
+      case DbTables.ingredient:
         return Ingredient.fromMap(data);
       default:
         return null;
