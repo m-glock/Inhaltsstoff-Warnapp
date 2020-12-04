2e2ac4dc949199e714679a1ef7a105d183b9a622
NoraAnlima <nokiessling@googlemail.com>
2020-12-04 16:23:08 +0100

    replaced GestureDetector onTab with onTab of ListTile

--- lib/pages/settings/SettingsRoot.dart
+++ lib/pages/settings/SettingsRoot.dart
@@ -38,21 +38,19 @@ class _SettingsRootState extends State<SettingsRoot> {
       backgroundColor: Colors.white,
       body: ListView(
         children: allSettingsPages.map((SettingsPage settingsPage) {
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext context) => settingsPage.page));
-            },
-            child: ListTile(
+          return ListTile(
               leading: Icon(settingsPage.icon,
                   color: Theme.of(context).primaryColor),
               title: Text(settingsPage.title,
                   style: Theme.of(context).textTheme.headline2),
               trailing: Icon(Icons.keyboard_arrow_right,
                   color: Theme.of(context).primaryColor),
-            ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => settingsPage.page));
+              },
           );
         }).toList(),
       ),
