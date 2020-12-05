5a8bfdc472ad4db98d0ba4534239dd1fe3104714
m-glock <mareike.glock@t-online.de>
2020-12-05 15:49:53 +0100

    error handling for API access

--- lib/backend/FoodApiAccess.dart
+++ lib/backend/FoodApiAccess.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:io';
 
 import 'Product.dart';
 import 'package:http/http.dart' as http;
@@ -13,7 +14,7 @@ class FoodApiAccess{
   /*
   * sends an GET request to the food database API with the scanned barcode and creates a Product object containing the relevant information
   * @param barcode: the scanned barcode from a product
-  * @return: an object for the scanned product with the relevant information
+  * @return: an object for the scanned product with the relevant information or null if not found
   * */
   static Future<Product> scanProduct(String barcode) async{
     String requestUrl = '$_foodDbApiUrl/$_productEndpoint/$barcode.json';
@@ -21,10 +22,9 @@ class FoodApiAccess{
     http.Response response = await _getRequest(requestUrl);
     int status = response.statusCode;
 
-    // check if API returned a valid response
-    // TODO: better handling than just throwing exceptions?
-    if(status == 502 || status == 503 || status == 500) throw Exception('Open Food Facts API Server is down.');
-    else if(status != 200) return null;
+    // check HTTP status
+    if (status == 404) return null;
+    else if(status != 200) throw HttpException('$status');
 
     // handle if product does not exist in the food API database
     // utf8 decoding for umlaute
@@ -41,7 +41,7 @@ class FoodApiAccess{
   /*
   * sends a GET request to the food database API to get all possible values for a tag in a product json
   * @param tag: the tag name from the product json
-  * @return a List of all possible values that exist in the API for this specific tag
+  * @return a List of all possible values that exist in the API for this specific tag or null if tag was not found
   * */
   static Future<List<String>> getValuesForTag(String tag) async {
     String requestUrl = '$_foodDbApiUrl/$_taxonomyEndpoint/$tag.json';
@@ -49,14 +49,15 @@ class FoodApiAccess{
     http.Response response = await _getRequest(requestUrl);
     int status = response.statusCode;
 
-    //TODO: error handling for wrong status: body is html instead of json when searching for something that does not exist
-    // utf8 decoding for umlaute
-    Map<String, dynamic> decodedJson = json.decode(utf8.decode(response.bodyBytes));
-    List<String> possibleValuesForTag = new List();
+    // check HTTP status
+    if (status == 404) return null;
+    else if(status != 200) throw HttpException('$status');
 
+    // utf8 decoding for umlaute
     // if requested tag does not exist, the response body will be html and the decoding will return null
-    if(status == 502 || status == 503 || status == 500) throw Exception('Open Food Facts API Server is down.');
-    else if(status == 404) return null;
+    Map<String, dynamic> decodedJson = json.decode(utf8.decode(response.bodyBytes));
+    if(decodedJson == null) throw HttpException('404');
+    List<String> possibleValuesForTag = new List();
 
     decodedJson.forEach((key, value) {
       String tagValue = value['name']['de'];
@@ -72,7 +73,7 @@ class FoodApiAccess{
   * @param tag: the tag from the product json that the value to translate belongs to
   * @param tagValue: the name of the value to translate
   * @param languageCode: the ISO 639-1 language code for the target language
-  * @return the translation of the tag Value according to the API
+  * @return the translation of the tag Value according to the API or null if tag was not found
   * */
   static Future<List<String>> translateTagNames(String tag, List<dynamic> tagValues, {String languageCode:'de'}) async {
     String requestUrl = '$_foodDbApiUrl/$_taxonomyEndpoint/$tag.json';
@@ -80,14 +81,13 @@ class FoodApiAccess{
     http.Response response = await _getRequest(requestUrl);
     int status = response.statusCode;
 
-    //TODO: error handling for wrong status: body is html instead of json when searching for something that does not exist
+    // check HTTP status
+    if (status == 404) return null;
+    else if(status != 200) throw HttpException('$status');
+
     // utf8 decoding for umlaute
     Map<String, dynamic> decodedJson = json.decode(utf8.decode(response.bodyBytes));
     List<String> translatedTagValues = List();
-
-    // if requested tag does not exist, the response body will be html and the decoding will reutrn null
-    if(status == 502 || status == 503 || status == 500) throw Exception('Open Food Facts API Server is down.');
-    else if(status == 404) return null;
 
     tagValues.forEach((element) {
       String translatedName;
