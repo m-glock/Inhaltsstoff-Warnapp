dea94e9bde9f97b477a86f9a6e21121281d54fe5
m-glock <mareike.glock@t-online.de>
2020-12-06 18:21:53 +0100

    added method signatures and dummy data

--- lib/backend/Ingredient.dart
+++ lib/backend/Ingredient.dart
@@ -1,40 +1,37 @@
+import 'PreferenceType.dart';
 import 'Type.dart';
 import 'database/DbTable.dart';
 import 'database/DbTableNames.dart';
 import 'package:intl/intl.dart';
-import 'dart:convert';
 
 class Ingredient extends DbTable {
   // Fields
-  //int id;
-  String name;
-  int preferencesTypeId;
-  String addDate;
-
-  //TODO types?
-  //List<dynamic> groups;
+  String _name;
+  PreferenceType _preferencesType;
+  String _addDate;
+  List<Type> _types;
 
   static final columns = ["id", "name", "preferencesTypeId", "addDate"];
 
   // Constructor
-  //Ingredient(this._groups, this._name, {int id}) : super(id);
-  Ingredient(this.name, this.preferencesTypeId, this.addDate, {int id})
+  Ingredient(this._name, this._preferencesType, this._addDate, {int id})
       : super(id);
 
   // Getter and Setter
-  //String get name => _name;
-  //List<Type> get group => _groups;
+  String get name => _name;
+  List<Type> get types => _types;
+  String get addDate => _addDate;
+  PreferenceType get preferenceType => _preferencesType;
 
   // Methods
   DbTableNames getTableName() {
     return DbTableNames.ingredient;
   }
 
-  // TODO groups/group ids => how to handle joinTables or one to many relations both to and from database
   Map<String, dynamic> toMap({bool withId: true}) {
     final map = new Map<String, dynamic>();
     map['name'] = name;
-    map['preferencesTypeId'] = preferencesTypeId;
+    //map['preferencesTypeId'] = preferencesTypeId;
     map['addDate'] = addDate;
     if (withId) map['id'] = super.id;
     //List<int> groupIds = new List();
@@ -43,31 +40,28 @@ class Ingredient extends DbTable {
     return map;
   }
 
-  /*static fromMap(Map map) {
-    Ingredient ingredient = new Ingredient();
-    ingredient.id = map["id"];
-    ingredient.name = map["username"];
-    ingredient.preferencesTypeId = map["preferencesTypeId"];
-    ingredient.addDate = map["addDate"];
-
-    return ingredient;
-  }*/
-
-  factory Ingredient.fromMap(Map<String, dynamic> data) =>
+  static fromMap(Map<String, dynamic> data) =>
       new Ingredient(data['name'], data['preferencesTypeId'], data['addDate'],
           id: data['id']);
 
-  //add dateNow to
-  formatDate(addDate) {
+  /*
+  * get current date in a string format
+  * from https://stackoverflow.com/questions/16126579/how-do-i-format-a-date-with-dart
+  * @return: current date as a string in format yyyy-MM-dd-Hm
+  * */
+  String getCurrentDate() {
     final DateTime now = DateTime.now();
-    //https://stackoverflow.com/questions/16126579/how-do-i-format-a-date-with-dart
-    final DateFormat formatter = DateFormat('yyyy-MM-dd-Hms');
-    final String formatted = formatter.format(now);
-    addDate = formatted;
-    return addDate;
+    final DateFormat formatter = DateFormat('yyyy-MM-dd-Hm');
+    return formatter.format(now);
     //print(formatted); // something like 2013-04-20
   }
 
-  //TODO implement
-  changePreference(PreferenceType) {}
+  /*
+  * changes the preference type of this ingredient
+  * @param preferenceType: the new preference for this ingredient
+  * */
+  void changePreference(PreferenceType preferenceType) {
+    //TODO implement
+
+  }
 }
--- lib/backend/PreferenceManager.dart
+++ lib/backend/PreferenceManager.dart
@@ -1,7 +1,8 @@
 import 'dart:core';
 
-import 'package:Inhaltsstoff_Warnapp/backend/Ingredient.dart';
-import 'package:Inhaltsstoff_Warnapp/backend/PreferenceType.dart';
+import 'Ingredient.dart';
+import 'PreferenceType.dart';
+import 'Type.dart';
 
 import 'database/DbTable.dart';
 import 'database/DbTableNames.dart';
@@ -10,29 +11,54 @@ import 'database/databaseHelper.dart';
 class PreferenceManager {
   final dbHelper = DatabaseHelper.instance;
 
-  static changePreference(Ingredient, PreferenceType) {}
+  /*
+  * change the preference of one ingredient
+  * @param ingredient: the ingredient where the preference type should change
+  * @param preferenceType: the preferenceType to change to
+  * */
+  static void changePreference(Ingredient ingredient, PreferenceType preferenceType) {
+    // TODO implement
+  }
 
-  static getPreferencedIngredients() async {
-    List<Ingredient> ingredients;
+  /*
+  * returns all Ingredients that have a preferenceType that is not NONE
+  * @param preferenceTypes: if only ingredients with a specific preference type are requested
+  * @return: a list of all ingredients that the user has preferenced
+  * */
+  static List<Ingredient> getPreferencedIngredients({List<PreferenceType> preferenceTypes}) {
+    //TODO implement
+    List<Ingredient> ingredients = List();
+    ingredients.add(Ingredient('Zucker', PreferenceType.Unwanted, ''));
+    ingredients.add(Ingredient('Milch', PreferenceType.Unwanted, ''));
+    ingredients.add(Ingredient('Magnesium', PreferenceType.NotPreferred, ''));
+    ingredients.add(Ingredient('Wasser', PreferenceType.Preferred, ''));
     //await dbHelper.readAll(DbTableNames.ingredient);
-
+    return ingredients;
   }
 
-  // TODO define
-/*getPreferencedIngredients(PreferenceType){
-}*/
-
-  //TODO what does it mean under available
-  static getAvailableIngredients() {
-    List<Ingredient> ingredients;
+  /*
+  * @param type: if only the ingredients of a specific type are relevant
+  * @return: all Ingredients that are available in the database
+  * */
+  static List<Ingredient> getAllAvailableIngredients({Type type}) {
+    //TODO implement
+    List<Ingredient> ingredients = List();
+    ingredients.add(Ingredient('Zucker', PreferenceType.None, ''));
+    ingredients.add(Ingredient('Milch', PreferenceType.Unwanted, ''));
+    ingredients.add(Ingredient('Magnesium', PreferenceType.None, ''));
+    ingredients.add(Ingredient('Wasser', PreferenceType.Preferred, ''));
+    return ingredients;
   }
 
-  //TODO available?
-  static getAvailableIngredientsOfType(Type) {
-    List<Ingredient> ingredients;
-  }
-  
-  static getIngredientsTypes() {
+  /*
+  * @return: a list of all available types in the DB
+  * */
+  static List<Type> getIngredientsTypes() {
+    //TODO implement
     List<Type> type;
+    type.add(Type('Vitamin'));
+    type.add(Type('Allergen'));
+    type.add(Type('Nutriment'));
+    return type;
   }
 }
