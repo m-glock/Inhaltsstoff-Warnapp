70e0dfc6b2b9bec0806a1d627b78b6fcc42a1a4a
m-glock <mareike.glock@t-online.de>
2020-12-06 18:34:52 +0100

    expanded some comment for better clarity which method does what

--- lib/backend/PreferenceManager.dart
+++ lib/backend/PreferenceManager.dart
@@ -12,16 +12,16 @@ class PreferenceManager {
   final dbHelper = DatabaseHelper.instance;
 
   /*
-  * change the preference of one ingredient
-  * @param ingredient: the ingredient where the preference type should change
-  * @param preferenceType: the preferenceType to change to
+  * When the user changes the preference of one or multiple Ingredients, this method is called and
+  * saves the information in the database
+  * @param preferenceChanges: a map of ingredients and the preference types they should change to
   * */
-  static void changePreference(Ingredient ingredient, PreferenceType preferenceType) {
+  static void changePreference(Map<Ingredient, PreferenceType> preferenceChanges) {
     // TODO implement
   }
 
   /*
-  * returns all Ingredients that have a preferenceType that is not NONE
+  * get all Ingredients that are saved in the DB that do not have the preferenceType NONE
   * @param preferenceTypes: if only ingredients with a specific preference type are requested
   * @return: a list of all ingredients that the user has preferenced
   * */
@@ -37,7 +37,8 @@ class PreferenceManager {
   }
 
   /*
-  * @param type: if only the ingredients of a specific type are relevant
+  * Get all Ingredients from the DB independent of their preference type
+  * @param type: if only the ingredients of a specific type are relevant (i.e. vitamins, allergens)
   * @return: all Ingredients that are available in the database
   * */
   static List<Ingredient> getAllAvailableIngredients({Type type}) {
@@ -51,6 +52,7 @@ class PreferenceManager {
   }
 
   /*
+  * get a list of all ingredient types (i.e. vitamin, allergen etc.)
   * @return: a list of all available types in the DB
   * */
   static List<Type> getIngredientsTypes() {
