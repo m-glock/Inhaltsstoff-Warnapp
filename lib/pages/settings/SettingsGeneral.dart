be4d4bb387d23764e8084cd99ea8c9b90079c5ce
NoraAnlima <nokiessling@googlemail.com>
2020-12-08 17:27:05 +0100

    added ui elements (for now without functionality) to settings general page

--- lib/pages/settings/SettingsGeneral.dart
+++ lib/pages/settings/SettingsGeneral.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
-class SettingsGeneral extends StatelessWidget {
+class SettingsGeneral extends StatefulWidget {
   const SettingsGeneral({Key key,}) : super(key: key);
+
+  @override
+  _SettingsGeneralState createState() => _SettingsGeneralState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _SettingsGeneralState extends State<SettingsGeneral> {
+  double _currentTextSize;
+
+  List<String> _availableLanguages = ['Deutsch', 'English'];
+  String _currentLaguage = 'Deutsch';
+
+  @override
+  void initState() {
+    super.initState();
+    //TODO: get currentTextSize
+    _currentTextSize = 14;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +29,52 @@ class SettingsGeneral extends StatelessWidget {
         backgroundColor: Theme.of(context).primaryColor,
       ),
       backgroundColor: Colors.white,
-      body: Container(
-        padding: const EdgeInsets.all(32.0),
-        alignment: Alignment.center,
-        child: Text('Hier kann man die allgemeinen Einstellungen vornehmen.'),
+      body: Column(
+        children: [
+          ExpansionTile(
+            title: Text(
+              'Textgröße', 
+              style: Theme.of(context).textTheme.headline2
+            ),
+            children: [
+              Slider(
+                  value: _currentTextSize,
+                  min: 10,
+                  max: 20,
+                  divisions: 10,
+                  label: _currentTextSize.toString(),
+                  onChanged: (double value) {
+                    setState(() {
+                      //TODO: actually set text size
+                      _currentTextSize = value;
+                    });
+                  }
+              )
+            ],
+            initiallyExpanded: true,
+          ),
+          ExpansionTile(
+            title: Text(
+              'Sprache',
+              style: Theme.of(context).textTheme.headline2
+            ),
+            children: _availableLanguages.map((language) {
+              return RadioListTile(
+                title: Text(language, style: Theme.of(context).textTheme.bodyText1),
+                value: language,
+                selected: language == _currentLaguage,
+                groupValue: _currentLaguage,
+                onChanged: (String value) {
+                  setState(() {
+                    _currentLaguage = value;
+                  });
+                },
+                activeColor: Theme.of(context).primaryColor,
+              );
+            }).toList(),
+            initiallyExpanded: true,
+          )
+        ],
       ),
     );
   }
