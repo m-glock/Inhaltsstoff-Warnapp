cdda12229cf0531d0e7568f4723702be21c75c84
kipmann <kirill_prahov@mail.ru>
2020-12-16 11:41:14 +0100

    Update PreferenceType.dart

--- lib/backend/Enums/PreferenceType.dart
+++ lib/backend/Enums/PreferenceType.dart
@@ -1,16 +1,16 @@
 enum PreferenceType { NotWanted, NotPreferred, Preferred, None }
 
 extension PreferenceTypeExtension on PreferenceType {
-  int get name {
+  String get name {
     switch (this) {
       case PreferenceType.NotWanted:
-        return 1;//'notwanted';
+        return 'Not Wanted';
       case PreferenceType.NotPreferred:
-        return 2;//'notpreferred';
+        return 'Not Preferred';
       case PreferenceType.Preferred:
-        return 3;//'preferred';
+        return 'Preferred';
       case PreferenceType.None:
-        return 4;//'none';
+        return 'None';
       default:
         return null;
     }
