50067fd490f7d1269f30170a950c67bc70d41150
m-glock <mareike.glock@t-online.de>
2020-12-29 14:48:49 +0100

    get id method for PreferenceType

--- lib/backend/Enums/PreferenceType.dart
+++ lib/backend/Enums/PreferenceType.dart
@@ -15,4 +15,23 @@ extension PreferenceTypeExtension on PreferenceType {
         return null;
     }
   }
+
+  /*
+  * return the id that the enum has in the DB
+  * defined in assets/database/insert_into_tables_sql.txt
+  * */
+  int get id {
+    switch (this) {
+      case PreferenceType.None:
+        return 1;
+      case PreferenceType.NotWanted:
+        return 2;
+      case PreferenceType.NotPreferred:
+        return 3;
+      case PreferenceType.Preferred:
+        return 4;
+      default:
+        return null;
+    }
+  }
 }
