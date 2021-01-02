57f94572903b176a6066a11a9bf3407dbd9b5400
kipmann <kirill_prahov@mail.ru>
2021-01-02 17:11:58 +0100

    implement method getItemizedScanResults

--- lib/backend/PreferenceManager.dart
+++ lib/backend/PreferenceManager.dart
@@ -15,7 +15,8 @@ class PreferenceManager {
   * saves the information in the database
   * @param preferenceChanges: a map of ingredients and the preference types they should change to
   * */
-  static void changePreference(Map<Ingredient, PreferenceType> preferenceChanges) async {
+  static void changePreference(
+      Map<Ingredient, PreferenceType> preferenceChanges) async {
     final dbHelper = DatabaseHelper.instance;
     final db = await dbHelper.database;
 
@@ -47,7 +48,8 @@ class PreferenceManager {
   * @param preferenceTypes: if only ingredients with a specific preference type are requested
   * @return: a list of all ingredients that the user has preferred
   * */
-  static Future<List<Ingredient>> getPreferencedIngredients([List<PreferenceType> preferenceTypes]) async {
+  static Future<List<Ingredient>> getPreferencedIngredients(
+      [List<PreferenceType> preferenceTypes]) async {
     final dbHelper = DatabaseHelper.instance;
     final db = await dbHelper.database;
     List<Ingredient> ingredients = new List();
@@ -86,7 +88,8 @@ class PreferenceManager {
   * @param type: if only the ingredients of a specific type are relevant (i.e. vitamins, allergens)
   * @return: all Ingredients that are available in the database
   * */
-  static Future<List<Ingredient>> getAllAvailableIngredients([Type type]) async {
+  static Future<List<Ingredient>> getAllAvailableIngredients(
+      [Type type]) async {
     final dbHelper = DatabaseHelper.instance;
     final db = await dbHelper.database;
     List<Ingredient> ingredients = new List();
@@ -115,60 +118,74 @@ class PreferenceManager {
     }
 
     return ingredients;
-
   }
 
   /*
+  * TODO: test & improve the results -> discuss in team
   * Get all preferred ingredients and assign them a ScanResult on whether they are in the product or not.
   * Red = Ingredient is not wanted, but is contained in the product
   * Yellow = Ingredient is not preferred, but is contained in the product
-  * Green = Ingredient is neither preferred or wanted, but is not contained in the product
+  * Green = Ingredient is preferred or none and is contained in the product, or there is no match in the database
   *
   * @param product: the product whose ingredient should be compared to the preferred ingredients
   * @return: a Map for each preferred Ingredient and its respective ScanResult to the product
   * */
-  static Future<Map<Ingredient, ScanResult>> getItemizedScanResults(Product product) async {
-    Map <Ingredient, ScanResult> itemizedScanResults = Map();
+  static Future<Map<Ingredient, ScanResult>> getItemizedScanResults(
+      Product product) async {
+    Map<Ingredient, ScanResult> itemizedScanResults = Map();
     final dbHelper = DatabaseHelper.instance;
     final db = await dbHelper.database;
+    String productname = product.name;
+    String notwanted = 'Not Wanted';
+    String notpreferred = 'Not Preferred';
+    String preferred = 'Preferred';
+    String none = 'None';
 
     // red
-    String productname = product.name;
+    List<Map> results_red = await db.rawQuery(
+        "select i.name as ingredientName, p.name as preferenceName, i.preferenceAddDate, t.name TypeName, i.id from ingredient i join preferencetype p on i.preferenceTypeId=p.id join type t on i.typeId=t.id join productingredient pi on i.id=pi.ingredientId join product pr on pr.id=pi.productId where pr.name = ? and p.name = ?",
+        [productname, notwanted]);
+    if (results_red != null) {
+      results_red.forEach((result) {
+        //print(result);
+        Ingredient ingredient = Ingredient.fromMap(result);
+        ScanResult scanresult = ScanResult.Red;
+        itemizedScanResults.putIfAbsent(ingredient, () => scanresult);
+      });
+    }
 
-    List<Map> results = await db.rawQuery(
-        "select i.name as ingredientName, p.name as preferenceName, i.preferenceAddDate, t.name TypeName, i.id from ingredient i join preferencetype p on i.preferenceTypeId=p.id join type t on i.typeId=t.id join productingredient pi on i.id=pi.ingredientId join product pr on pr.id=pi.productId where pr.name = ?",[productname]);
-    // CREATE TABLE ingredient ( id INTEGER PRIMARY KEY AUTOINCREMENT, preferenceTypeId INTEGER DEFAULT NULL, name TEXT NOT NULL, preferenceAddDate DATE DEFAULT NULL, typeId INTEGER NOT NULL, FOREIGN KEY(preferenceTypeId) REFERENCES preferencetype(id), FOREIGN KEY(typeId) REFERENCES type(id));
-    // CREATE TABLE scanresult ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL );
-    // CREATE TABLE list ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, parent INTEGER DEFAULT NULL, FOREIGN KEY(parent) REFERENCES list(id) );
-    // CREATE TABLE product ( id INTEGER PRIMARY KEY AUTOINCREMENT, scanResultId INTEGER NOT NULL, name TEXT NOT NULL, imageUrl TEXT DEFAULT NULL, barcode TEXT NOT NULL, scanDate INTEGER NOT NULL, lastUpdated INTEGER DEFAULT NULL, nutriScore INTEGER DEFAULT NULL, quantity INTEGER DEFAULT NULL, originCountry TEXT DEFAULT NULL, manufactoringPlaces TEXT DEFAULT NULL, stores TEXT DEFAULT NULL, FOREIGN KEY(scanResultId) REFERENCES scanresult(id) );CREATE TABLE productlist ( listId INTEGER NOT NULL, productId INTEGER NOT NULL, FOREIGN KEY(listId) REFERENCES list(id), FOREIGN KEY(productId) REFERENCES product(id) );
-    // CREATE TABLE productingredient ( productId INTEGER NOT NULL, ingredientId INTEGER NOT NULL,
-    results.forEach((result) {
-      print(result);
-      Ingredient ingredient = Ingredient.fromMap(result);
-      ScanResult scanresult = ScanResult.Red;
+    // yellow
+    List<Map> results_yellow = await db.rawQuery(
+        "select i.name as ingredientName, p.name as preferenceName, i.preferenceAddDate, t.name TypeName, i.id from ingredient i join preferencetype p on i.preferenceTypeId=p.id join type t on i.typeId=t.id join productingredient pi on i.id=pi.ingredientId join product pr on pr.id=pi.productId where pr.name = ? and p.name = ?",
+        [productname, notpreferred]);
+    if (results_yellow != null) {
+      results_yellow.forEach((result) {
+        //print(result);
+        Ingredient ingredient = Ingredient.fromMap(result);
+        ScanResult scanresult = ScanResult.Yellow;
+        itemizedScanResults.putIfAbsent(ingredient, () => scanresult);
+      });
+    }
 
-      //ingredients.add(ingredient);
-    });
-
+    // green
+    List<Map> results_green = await db.rawQuery(
+        "select i.name as ingredientName, p.name as preferenceName, i.preferenceAddDate, t.name TypeName, i.id from ingredient i join preferencetype p on i.preferenceTypeId=p.id join type t on i.typeId=t.id join productingredient pi on i.id=pi.ingredientId join product pr on pr.id=pi.productId where pr.name = ? and (p.name = ? or p.name = ?)",
+        [productname, preferred, none]);
+    if (results_green != null) {
+      results_green.forEach((result) {
+        //print(result);
+        Ingredient ingredient = Ingredient.fromMap(result);
+        ScanResult scanresult = ScanResult.Green;
+        itemizedScanResults.putIfAbsent(ingredient, () => scanresult);
+      });
+    }
 
     return itemizedScanResults;
   }
 
-    // yellow
-
-    // green
-
-
-
-
-      //retrieve id from the new preferenceType
-
-
-
-    // itemizedScanResults[Ingredient('Zucker', PreferenceType.NotWanted, '')] = ScanResult.Red; //not wanted and in product
-    // itemizedScanResults[Ingredient('Schokolade', PreferenceType.NotPreferred, '')] = ScanResult.Yellow; // not preferred and in product
-    // itemizedScanResults[Ingredient('Magnesium', PreferenceType.NotPreferred, '')] = ScanResult.Green; //not preferred and not in product
-    //itemizedScanResults[Ingredient('Wasser', PreferenceType.Preferred, '')] = ScanResult.Red; //preferred and not in product
-
+// itemizedScanResults[Ingredient('Zucker', PreferenceType.NotWanted, '')] = ScanResult.Red; //not wanted and in product
+// itemizedScanResults[Ingredient('Schokolade', PreferenceType.NotPreferred, '')] = ScanResult.Yellow; // not preferred and in product
+// itemizedScanResults[Ingredient('Magnesium', PreferenceType.NotPreferred, '')] = ScanResult.Green; //not preferred and not in product
+//itemizedScanResults[Ingredient('Wasser', PreferenceType.Preferred, '')] = ScanResult.Red; //preferred and not in product
 
 }
