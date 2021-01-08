afd0cbf58e6b99ca5001912efc4b938fd0eb4d84
m-glock <mareike.glock@t-online.de>
2021-01-08 17:49:15 +0100

    implemented methods to manage favourite products and categories

--- lib/backend/Lists/FavouriteList.dart
+++ lib/backend/Lists/FavouriteList.dart
@@ -4,7 +4,7 @@ import '../Product.dart';
 class FavouriteList extends ProductList{
 
   // Fields
-  static Set<Product> _favouriteProducts;
+  static Map<Product, String> _favouriteProducts;
   static List<String> _favouriteCategories;
 
   // Getter
@@ -13,27 +13,36 @@ class FavouriteList extends ProductList{
 
   // Constructor
   FavouriteList({int id}) : super(id){
-    _favouriteProducts = Set();
+    _favouriteProducts = Map();
     _favouriteCategories = List();
   }
 
   // Methods
-  void addToFavourites(Product product){
-    _favouriteProducts.add(product);
+  bool addToFavourites(Product product, {String category = 'None'}){
+    if(!_favouriteCategories.contains(category)) _favouriteCategories.add(category);
+    if(_favouriteProducts.containsKey(product)) return false;
+
+    _favouriteProducts[product] = category;
+    return true;
   }
 
   void removeFromFavourites(Product product){
-    _favouriteProducts.removeWhere((element) => element.equals(product));
+    _favouriteProducts.removeWhere((key, value) => key.equals(product));
   }
 
-  void addCategory(String name){
-    // TODO: implement addCategory
-    throw UnimplementedError();
+  void changeCategoryOfProduct(Product product, String newCategory){
+    _favouriteProducts[product] = newCategory;
+  }
+
+  bool addCategory(String name){
+    if(_favouriteCategories.contains(name)) return false;
+
+    _favouriteCategories.add(name);
+    return true;
   }
 
   void removeCategory(String name) {
-    // TODO: implement removeCategory
-    throw UnimplementedError();
+    _favouriteCategories.remove(name);
   }
 
 }
