ca038f6d8f5e2082f185520fdee66c5496aac4ab
NoraAnlima <nokiessling@googlemail.com>
2021-01-13 01:05:46 +0100

    implemented not preferenced ingredients and additional product info sections of comparison view

--- lib/pages/comparison/ComparisonViewPage.dart
+++ lib/pages/comparison/ComparisonViewPage.dart
@@ -1,4 +1,5 @@
 import 'package:Inhaltsstoff_Warnapp/backend/Enums/ScanResult.dart';
+import 'package:Inhaltsstoff_Warnapp/backend/Ingredient.dart';
 import 'package:Inhaltsstoff_Warnapp/backend/PreferenceManager.dart';
 import 'package:Inhaltsstoff_Warnapp/customWidgets/ResultCircle.dart';
 import 'package:Inhaltsstoff_Warnapp/pages/comparison/comparisonCustomWidgets/ComparisonSelectedProductCard.dart';
@@ -6,7 +7,7 @@ import 'package:flutter/material.dart';
 
 import '../../backend/Product.dart';
 
-class ComparisonViewPage extends StatelessWidget {
+class ComparisonViewPage extends StatefulWidget {
   const ComparisonViewPage({
     Key key,
     this.productOne,
@@ -17,131 +18,234 @@ class ComparisonViewPage extends StatelessWidget {
   final Product productTwo;
 
   @override
-  Widget build(BuildContext context) {
+  _ComparisonViewPageState createState() => _ComparisonViewPageState();
+}
+
+class _ComparisonViewPageState extends State<ComparisonViewPage> {
+  Map<Ingredient, ScanResult> _itemizedResultsProductOne;
+  Map<Ingredient, ScanResult> _itemizedResultsProductTwo;
+
+  List<Ingredient> _preferences;
+
+  List<String> _notPreferencedIngredientsSum;
+  List<String> _notPreferencedIngredientsProductOne;
+  List<String> _notPreferencedIngredientsProductTwo;
+
+  Map<String, String> _additionalDetailsProductOne;
+  Map<String, String> _additionalDetailsProductTwo;
+
+  @override
+  void initState() {
+    super.initState();
+    _getItemizedResults();
+
+    _notPreferencedIngredientsProductOne =
+        widget.productOne.getNotPreferredIngredientNames();
+    _notPreferencedIngredientsProductTwo =
+        widget.productTwo.getNotPreferredIngredientNames();
+
+    var notPreferencedIngredientsSet = new Set<String>();
+    notPreferencedIngredientsSet.addAll(_notPreferencedIngredientsProductOne);
+    notPreferencedIngredientsSet.addAll(_notPreferencedIngredientsProductTwo);
+    _notPreferencedIngredientsSum = notPreferencedIngredientsSet.toList();
+
+    _additionalDetailsProductOne =
+        _getAdditionalProductDetails(widget.productOne);
+    _additionalDetailsProductTwo =
+        _getAdditionalProductDetails(widget.productTwo);
+  }
+
+  void _getItemizedResults() async {
     var resultsProductOne =
-        PreferenceManager.getItemizedScanResults(productOne);
+        await PreferenceManager.getItemizedScanResults(widget.productOne);
+    print('results product one: ' + resultsProductOne.toString());
     var resultsProductTwo =
-        PreferenceManager.getItemizedScanResults(productTwo);
+        await PreferenceManager.getItemizedScanResults(widget.productTwo);
+    print('results product two: ' + resultsProductTwo.toString());
+    var preferences = await PreferenceManager.getPreferencedIngredients();
+    print('preferences: ' + preferences.toString());
+    setState(() {
+      _itemizedResultsProductOne = resultsProductOne;
+      _itemizedResultsProductTwo = resultsProductTwo;
+      _preferences = preferences;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return ListView(
       padding: EdgeInsets.all(24.0),
       children: <Widget>[
-        Stack(
-          children: [
-            /*Positioned.fill(
-              child: IntrinsicHeight(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                      child: ColoredBox(
-                        color: Colors.white,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: ColoredBox(
-                        color: Theme.of(context).accentColor,
-                      ),
-                    ),
-                    Expanded(
-                      child: ColoredBox(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
+        Padding(
+          padding: EdgeInsets.only(top: 10.0, bottom: 24.0),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: ComparisonSelectedProductCard(
+                  productNumber: 1,
+                  productName: widget.productOne.name,
+                  scanDate: widget.productOne.scanDate,
+                  showInfoButton: false,
                 ),
               ),
-            ),*/
-            Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0, bottom: 24.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: ComparisonSelectedProductCard(
-                          productNumber: 1,
-                          productName: productOne.name,
-                          scanDate: productOne.scanDate,
-                          showInfoButton: false,
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: ComparisonSelectedProductCard(
-                          productNumber: 2,
-                          productName: productTwo.name,
-                          scanDate: productTwo.scanDate,
-                          showInfoButton: false,
-                        ),
-                      ),
-                    ],
-                  ),
+              Expanded(
+                flex: 1,
+                child: ComparisonSelectedProductCard(
+                  productNumber: 2,
+                  productName: widget.productTwo.name,
+                  scanDate: widget.productTwo.scanDate,
+                  showInfoButton: false,
                 ),
-                ExpansionTile(
-                  title: Text(
-                    'Deine Präferenzen',
-                    style: Theme.of(context).textTheme.headline2,
-                    //textAlign: TextAlign.center,
-                  ),
-                  initiallyExpanded: true,
-                  children: PreferenceManager.getPreferencedIngredients()
-                      .map((preference) {
-                    return IntrinsicHeight(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Expanded(
-                            child: ResultCircle(
-                              result: resultsProductOne[preference],
-                              small: true,
-                            ),
+              ),
+            ],
+          ),
+        ),
+        ExpansionTile(
+          title: Text(
+            'Deine Präferenzen',
+            style: Theme.of(context).textTheme.headline2,
+            //textAlign: TextAlign.center,
+          ),
+          initiallyExpanded: true,
+          children: _preferences == null ||
+                  _itemizedResultsProductOne == null ||
+                  _itemizedResultsProductTwo == null
+              ? [
+                  CircularProgressIndicator(),
+                ]
+              : _preferences.map((preference) {
+                  return IntrinsicHeight(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                          child: ResultCircle(
+                            result: _itemizedResultsProductOne[preference],
+                            small: true,
                           ),
-                          Expanded(
-                            flex: 3,
-                            child: ColoredBox(
-                              color: Theme.of(context).accentColor,
-                              child: Center(
-                                child: Text(
-                                  preference.name,
-                                  style: Theme.of(context).textTheme.bodyText1,
-                                ),
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: ColoredBox(
+                            color: Theme.of(context).accentColor,
+                            child: Center(
+                              child: Text(
+                                preference.name,
+                                style: Theme.of(context).textTheme.bodyText1,
+                                textAlign: TextAlign.center,
                               ),
                             ),
                           ),
-                          Expanded(
-                            child: ResultCircle(
-                              result: resultsProductTwo[preference],
-                              small: true,
-                            ),
+                        ),
+                        Expanded(
+                          child: ResultCircle(
+                            result: _itemizedResultsProductTwo[preference],
+                            small: true,
                           ),
-                        ],
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+        ),
+        ExpansionTile(
+          title: Text(
+            'Andere Inhaltsstoffe',
+            style: Theme.of(context).textTheme.headline2,
+            //textAlign: TextAlign.center,
+          ),
+          initiallyExpanded: true,
+          children: _notPreferencedIngredientsSum.map((ingredient) {
+            return IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: _notPreferencedIngredientsProductOne
+                            .contains(ingredient)
+                        ? Icon(Icons.done)
+                        : Icon(Icons.clear),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: ColoredBox(
+                      color: Theme.of(context).accentColor,
+                      child: Center(
+                        child: Text(
+                          ingredient,
+                          style: Theme.of(context).textTheme.bodyText1,
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    );
-                  }).toList(),
-                ),
-                ExpansionTile(
-                  title: Text(
-                    'Andere Inhaltsstoffe',
-                    style: Theme.of(context).textTheme.headline2,
-                    //textAlign: TextAlign.center,
+                    ),
                   ),
-                  initiallyExpanded: true,
-                ),
-                ExpansionTile(
-                  title: Text(
-                    'Weitere Produktdetails',
-                    style: Theme.of(context).textTheme.headline2,
-                    //textAlign: TextAlign.center,
+                  Expanded(
+                    child: _notPreferencedIngredientsProductTwo
+                            .contains(ingredient)
+                        ? Icon(Icons.done)
+                        : Icon(Icons.clear),
                   ),
-                  initiallyExpanded: true,
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            );
+          }).toList(),
+        ),
+        ExpansionTile(
+          title: Text(
+            'Weitere Produktdetails',
+            style: Theme.of(context).textTheme.headline2,
+            //textAlign: TextAlign.center,
+          ),
+          initiallyExpanded: true,
+          children: _additionalDetailsProductOne.entries.map((detailProductOne) {
+            return IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Text(
+                      detailProductOne.value,
+                      style: Theme.of(context).textTheme.bodyText1,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: ColoredBox(
+                      color: Theme.of(context).accentColor,
+                      child: Center(
+                        child: Text(
+                          detailProductOne.key,
+                          style: Theme.of(context).textTheme.bodyText1,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      _additionalDetailsProductTwo[detailProductOne.key],
+                      style: Theme.of(context).textTheme.bodyText1,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
         ),
       ],
     );
+  }
+
+  Map<String, String> _getAdditionalProductDetails(Product product) {
+    return {
+      'Menge': product.quantity ?? '-',
+      'Herkunft': product.origin ?? '-',
+      'Herstellungsorte': product.manufacturingPlaces ?? '-',
+      'Geschäfte': product.stores ?? '-',
+      'Nutriscore': product.nutriscore ?? '-',
+    };
   }
 }
