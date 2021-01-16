44eba61ee9526955844f0c18f98e2f24753a09ab
Kathi <katharina.blass@student.hpi.de>
2021-01-16 19:57:21 +0100

    add logo and Appname to Welcome page

--- lib/frontend/pages/WelcomePage.dart
+++ lib/frontend/pages/WelcomePage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 import '../../backend/database/databaseHelper.dart';
 
@@ -12,10 +11,12 @@ class WelcomePage extends StatefulWidget {
 
 class _WelcomePageState extends State<WelcomePage> {
   waitforDatabaseBeforeRouting() async {
-    List<Map<String, dynamic>> map = await DatabaseHelper.instance.customQuery('SELECT * FROM metadataFlags WHERE name = \'Onboarding\'');
+    List<Map<String, dynamic>> map = await DatabaseHelper.instance
+        .customQuery('SELECT * FROM metadataFlags WHERE name = \'Onboarding\'');
     int isOnboardingDone = map[0]['isInitialized'] as int;
     if (isOnboardingDone == 0) {
-      await DatabaseHelper.instance.customQuery('UPDATE metadataFlags SET isInitialized=1 WHERE name = \'Onboarding\'');
+      await DatabaseHelper.instance.customQuery(
+          'UPDATE metadataFlags SET isInitialized=1 WHERE name = \'Onboarding\'');
       Navigator.of(context).pushReplacementNamed('/onboarding');
     } else {
       Navigator.of(context).pushReplacementNamed('/');
@@ -24,22 +25,46 @@ class _WelcomePageState extends State<WelcomePage> {
 
   @override
   Widget build(BuildContext context) {
-    new Timer(new Duration(milliseconds: 1000), () {
-      waitforDatabaseBeforeRouting();
-    });
+    waitforDatabaseBeforeRouting();
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              child: Text("Welcome!"),
-              padding: EdgeInsets.all(12.0),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            child: Text(
+              "Essbar",
+              style: Theme.of(context).textTheme.headline1.merge(
+                    TextStyle(
+                      fontSize: 44.0,
+                    ),
+                  ),
             ),
-            CircularProgressIndicator(),
-          ],
-        ),
+            padding: EdgeInsets.all(12.0),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 100.0,
+              vertical: 12.0,
+            ),
+            child: Image.asset(
+              'assets/images/logo.png',
+            ),
+          ),
+          Padding(
+            child: Text(
+                "Wir bereiten alles für dich vor. Bitte habe einen Moment Geduld!",
+                style: Theme.of(context).textTheme.bodyText1,
+                textAlign: TextAlign.center),
+            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 60.0),
+          ),
+          CircularProgressIndicator(
+            backgroundColor: Theme.of(context).accentColor,
+            valueColor: AlwaysStoppedAnimation<Color>(
+              Theme.of(context).primaryColor,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:Inhaltsstoff_Warnapp/backend/ListManager.dart';
 import 'package:flutter/material.dart';
 
 import './frontend/pages/HomePage.dart';
