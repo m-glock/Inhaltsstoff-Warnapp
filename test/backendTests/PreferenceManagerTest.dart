989dbf90dca5d444ff39d72c398deff7d82943e9
kipmann <kirill_prahov@mail.ru>
2021-01-23 14:20:56 +0100

    add new backend tests, correct the main structure for the run, add the instruction for the run

--- test/backendTests/PreferenceManagerTest.dart
+++ test/backendTests/PreferenceManagerTest.dart
@@ -1,19 +1,12 @@
-import 'package:Essbar/backend/Enums/ScanResult.dart';
 import 'package:Essbar/backend/PreferenceManager.dart';
 import 'package:Essbar/backend/Product.dart';
-import 'package:Essbar/main.dart';
-
-import '../../lib/backend/database/DatabaseHelper.dart';
+import 'package:Essbar/backend/database/DbTableNames.dart';
+import 'package:Essbar/backend/database/DatabaseHelper.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:Essbar/frontend/customWidgets/preferences/PreferencesAllergensView.dart';
 import 'package:Essbar/backend/Enums/PreferenceType.dart';
 import 'package:Essbar/backend/Enums/Type.dart';
 import 'package:Essbar/backend/Ingredient.dart';
 import 'dart:async';
-import '../../lib/main.dart' as app;
-import '../../lib/main.dart';
-//import 'package:test/test.dart';
-
 
 /*
 How to run the tests? Please read @HowToUse.txt
@@ -25,33 +18,37 @@ Future<void> main() async {
   test('get ingredients due to the preference type NotWanted', () async {
     List<PreferenceType> preferenceTypes = List<PreferenceType>();
     preferenceTypes.add(PreferenceType.NotWanted);
-    List <Ingredient> ingredients =
-    await PreferenceManager.getPreferencedIngredients(preferenceTypes);
-    print("expected value is not null:");
-    print(ingredients != null);
+    List<Ingredient> ingredients =
+        await PreferenceManager.getPreferencedIngredients(preferenceTypes);
+    //print("expected value is not null:");
+    //print(ingredients != null);
+    assert(ingredients != null);
   });
 
   test('get ingredients due to the preference type None', () async {
     List<PreferenceType> preferenceTypes = List<PreferenceType>();
     preferenceTypes.add(PreferenceType.None);
-    List <Ingredient> ingredients =
-    await PreferenceManager.getPreferencedIngredients(preferenceTypes);
-    print("expected value is not null:");
-    print(ingredients != null);
+    List<Ingredient> ingredients =
+        await PreferenceManager.getPreferencedIngredients(preferenceTypes);
+    //print("expected value is not null:");
+    //print(ingredients != null);
+    assert(ingredients != null);
   });
 
   test('get all available ingredients without a type', () async {
-    List <Ingredient> ingredients = await PreferenceManager
-        .getAllAvailableIngredients();
-    print("expected value is not null:");
-    print(ingredients != null);
+    List<Ingredient> ingredients =
+        await PreferenceManager.getAllAvailableIngredients();
+    //print("expected value is not null:");
+    //print(ingredients != null);
+    assert(ingredients != null);
   });
 
   test('get all available ingredients with a specific type', () async {
-    List <Ingredient> ingredients = await
-    PreferenceManager.getAllAvailableIngredients(Type.General);
-    print("expected value is not null:");
-    print(ingredients != null);
+    List<Ingredient> ingredients =
+        await PreferenceManager.getAllAvailableIngredients(Type.General);
+    //print("expected value is not null:");
+    //print(ingredients != null);
+    assert(ingredients != null);
   });
 
   test('get existing ingredient and change preference type', () async {
@@ -70,7 +67,6 @@ Future<void> main() async {
 
     //change preference
     PreferenceManager.changePreference(preferenceToChange);
-
     var resultSetIngredient1 = await dbHelper.customQuery(
         'select preferenceTypeId as pr_id from ingredient where name = "Hydroxocobalamin"');
     var dbItem1 = resultSetIngredient1.first;
@@ -78,9 +74,26 @@ Future<void> main() async {
     ['The values after change PreferenceTypeId:', prId1].forEach(print);
   });
 
-  //TODO check
-  test('get itemized scan results: red', () async {
-    //var ingredients = //PreferenceManager.getItemizedScanResults(Product(_name, _imageUrl, _barcode, _scanDate));
-    //assert(ingredients != null);
+  test('get itemized scan results for the not existing item', () async {
+    var scanResult = PreferenceManager.getItemizedScanResults(
+        Product("Milch", "_imageUrl", "111111111111", DateTime.now()));
+    //print("expected value is not null:");
+    //print(scanResult != null);
+    assert(scanResult != null);
+  });
+
+  test('get itemized scan results for existing item', () async {
+    Product product;
+    await dbHelper
+        .add(Product("Milch1", "_imageUrl", "2111111111111", DateTime.now()));
+    var products = await dbHelper.readAll(DbTableNames.product);
+    products.forEach((element) {
+      product = element;
+    });
+    var scanResult = PreferenceManager.getItemizedScanResults(
+        Product("Milch1", "_imageUrl", "2111111111111", DateTime.now()));
+    //print("expected value is not null:");
+    //print(scanResult != null);
+    assert(scanResult != null);
   });
 }
